e7ca8fe12e23bb5089ff09f46567ba60d2d3185e
Vivek Tripathi <114072519+Vivek-Tripathi00@users.noreply.github.com>
2025-04-29 20:01:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -660,7 +660,9 @@ def delete_registration(id):
 #registratuion form section deletion by admin code logic================================================================
 
 
-# Run the application
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
